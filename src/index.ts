5e72baa0a5c51aee6201894da0bc611594730452
raflytch <raflyazizabdillah30@gmail.com>
2024-10-11 02:23:21 +0700

    initialize project ts node

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+const port: Number = 3000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: "200" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
